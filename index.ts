9210cb59a66994cd19b51de3af6c98f04f9affcb
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-08-24 07:12:24 +0700

    Commit 04

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
 const commit_01 = 'Day la commit so 01'
 const commit_02 = 'Day la commit so 02'
 const commit_03 = 'Day la commit so 03'
+const commit_04 = 'Day la commit so 04'
+const commit_05 = 'Day la commit so 05'
+const commit_06 = 'Day la commit so 06'
